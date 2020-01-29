1ddf43354a3a43434eb496132b0e857ca75e5e77
Dmitry <dm.kolomiets@gmail.com>
2020-01-29 20:21:45 +0200

    add tab

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-alert(1)
+$(document).ready(function(){
+    $('.carousel-item').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            },
+        ]
+      });
+  });
